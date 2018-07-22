52d7463a97ad607e3ae3b5bb2c982d24444797da
0xmalik <arkina.x@gmail.com>
2018-07-22 19:18:11 +0200

    after adding passport_1

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+let User = require('./models/user');
+
+const mongoos = require('mongoose');
+mongoos.connect('mongodb://localhost/test');
+
+let db = mongoos.connection;
+db.once('open',function () {
+    console.log('connected to MongoDB');
+});
+db.on('error',function (err) {
+   console.log(err);
+});
+
 //view engine
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',function (req,res) {
     res.render('login');
@@ -17,6 +34,22 @@ app.get('/add-cust',function (req,res) {
 });
 app.get('/list-cust',function (req,res) {
     res.render('list-cust');
+});
+app.get('/add-user',function (req,res) {
+    res.render('add-user');
+});
+app.post('/add-user',function (req,res) {
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    let newUser = new User({
+        name:name,
+        username:username,
+        password:password
+    });
+});
+app.get('/list-user',function (req,res) {
+    res.render('list-user');
 });
 
 app.listen(3000, function () {
