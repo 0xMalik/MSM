ea39c85796f01074c05c5c4b192b45e22c4b0dc1
0xmalik <arkina.x@gmail.com>
2018-07-25 18:30:17 +0200

    Add views

--- app.js
+++ app.js
@@ -8,37 +8,46 @@ const mongoos = require('mongoose');
 mongoos.connect('mongodb://localhost/test');
 
 let db = mongoos.connection;
-db.once('open',function () {
+db.once('open', function () {
     console.log('connected to MongoDB');
 });
-db.on('error',function (err) {
-   console.log(err);
+db.on('error', function (err) {
+    console.log(err);
 });
 
 //view engine
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/',function (req,res) {
+app.get('/', function (req, res) {
     res.render('login');
 });
-app.get('/index',function (req,res) {
+app.get('/index', function (req, res) {
     res.render('index');
 });
-app.get('/add-cust',function (req,res) {
+app.get('/add-cust', function (req, res) {
     res.render('add-cust');
 });
-app.get('/list-cust',function (req,res) {
+app.get('/list-cust', function (req, res) {
     res.render('list-cust');
 });
-app.get('/add-user',function (req,res) {
+app.get('/add-user', function (req, res) {
     res.render('add-user');
 });
-app.post('/add-user',function (req,res) {
+app.get('/add-pack', function (req, res) {
+    res.render('add-pack');
+});
+app.get('/list-pack', function (req, res) {
+    res.render('list-pack');
+});
+app.get('/logs', function (req, res) {
+    res.render('logs');
+});
+/*app.post('/add-user',function (req,res) {
     const name = req.body.name;
     const username = req.body.username;
     const password = req.body.password;
@@ -47,11 +56,26 @@ app.post('/add-user',function (req,res) {
         username:username,
         password:password
     });
-});
-app.get('/list-user',function (req,res) {
+});*/
+app.post('/add-user', (req, res) => {
+    let user = new User();
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.password = req.body.password;
+    user.save(function (err) {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/add-user');
+        }
+    });
+    }
+);
+app.get('/list-user', function (req, res) {
     res.render('list-user');
 });
 
 app.listen(3000, function () {
-   console.log('Server Running on port http://localhost:3000 ... ');
+    console.log('Server Running on port http://localhost:3000 ... ');
 });
